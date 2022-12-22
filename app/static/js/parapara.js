997639795294236b3dce0c4396e204c172e2440c
DoubleMistence <guren1808@gmail.com>
2022-12-22 17:26:44 +0900

    "[Update]"

--- app/static/js/parapara.js
+++ app/static/js/parapara.js
@@ -2,7 +2,7 @@ $(function() {
     var imgs = new Array();
     var img_objects = new Array();
     var cnt = 18;       // 画像枚数
-    var speed = 50;   // ミリ秒(1秒=1000)
+    var speed = 90;   // ミリ秒(1秒=1000)
     var now = -1;
     var timerName;
     var result = document.querySelector("#result");
@@ -23,16 +23,13 @@ $(function() {
     for (i=0; i<cnt; i++) {
         imgs[i] = "../static/img/img" + (i+1) + ".PNG";
     }
-    console.log(imgs.length)
     loaded_imgs = preload_imgs(imgs);
-    console.log(loaded_imgs)
  
     //timerName = setInterval(pars2images, speed);   // 自動的に開始する場合はコメント外す
  
     // パラパラ実行
     function pars2images() {
         now++;
-        //$("#paraImage").attr("src", imgs[now]);
         $("#paraImage").attr('src', loaded_imgs[now].src);
         now_num = now + 1;
 
@@ -48,9 +45,7 @@ $(function() {
             timerName = null;
             result.textContent = Math.round(now_num/2);
             if (now_num%2 === 1) {
-                //$("#paraImage").attr("src", imgs[new_num]);
                 $("#paraImage").attr('src', loaded_imgs[now_num].src);
-                console.log(now_num%2);
             }
         } else {
             timerName = setInterval(pars2images, speed);
